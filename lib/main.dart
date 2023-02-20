2a4f6724aec8eb740d14842ab6ed52635413eee2
timofey fayzullin <89485758+dartik24@users.noreply.github.com>
2023-02-19 19:50:34 -0500

    second page added, scoring added, full display image on second page added, page to page navigation added, score calculation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GGrade Demo',
       theme: ThemeData(
         primarySwatch: Colors.orange
       ),
@@ -30,15 +31,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   List games = ["Atomic Heart", "Hogwarts Legacy", "Octopath II", "Kerbal Space Program II"];
-  List scores = [7,9,8,7];
   List img = [Image.asset('assets/AH.jpg'),Image.asset('assets/hgw.jpg'),Image.asset('assets/OT.jpg'),Image.asset('assets/KSP.jpg'),];
+  List dph = [5,5,8,10];
+  List perf = [7,6,9,7];
+  List user = [7,8,7,9];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  double find_score(int index){
+    double res = (dph[index] + perf[index] + user[index])/3;
+    return res;
   }
 
   @override
@@ -73,13 +74,15 @@ class _MyHomePageState extends State<MyHomePage> {
                   margin: const EdgeInsets.fromLTRB(5,5,5,5),
                   child: ListTile(
                     leading: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderRadius: const BorderRadius.all(Radius.circular(10.0)),
                       child: img[index],
                     ),
                     title: Text("${games[index]}", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
                     subtitle: Column(
                       children: <Widget>[
-                        FloatingActionButton.large(child: Text("${scores[index]}/10", style: TextStyle(fontWeight: FontWeight.bold),),onPressed: (){})
+                        FloatingActionButton.large( heroTag: "$index",child: Text("${find_score(index).toStringAsFixed(1)}/10", style: const TextStyle(fontWeight: FontWeight.bold),),onPressed: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => MyScorePage(title: "${games[index]}"),));
+                        })
                       ],
                     )
                   )
@@ -90,3 +93,217 @@ class _MyHomePageState extends State<MyHomePage> {
       ));
   }
 }
+
+class MyScorePage extends StatefulWidget {
+  const MyScorePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyScorePage> createState() => _MyScorePageState(title);
+}
+
+class _MyScorePageState extends State<MyScorePage> {
+  String title;
+  _MyScorePageState(this.title);
+  List games = ["Atomic Heart", "Hogwarts Legacy", "Octopath II", "Kerbal Space Program II"];
+  List img = ["assets/AH.jpg" , "assets/hgw.jpg", "assets/OT.jpg", "assets/KSP.jpg"];
+  List dph = [5,5,8,10];
+  List perf = [7,6,9,7];
+  List user = [7,8,7,9];
+
+  double find_score(int index){
+    double res = (dph[index] + perf[index] + user[index])/3;
+    return res;
+  }
+
+  int fullRun(String name){
+    for(int i = 0; i < games.length; i++) {
+      if(games[i]==name){
+        return i;
+      }
+    }
+    return 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+          theme: ThemeData(primarySwatch: Colors.orange),
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text(title),
+              centerTitle: true,
+            ),
+            body: DecoratedBox(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: const AssetImage("assets/background.png"),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(Colors.grey.withOpacity(0.5), BlendMode.color),
+                    )
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                      Container(
+                          margin: EdgeInsets.only(left: 10, right: 10, top: 10),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(color: Colors.black.withOpacity(0.25),
+                                blurRadius: 5,
+                                spreadRadius: 5,
+                                offset: const Offset(2.0, 2.0),)
+                            ],
+                            borderRadius: const BorderRadius.all(Radius.circular(15)),
+                          ),
+                          child: Image.asset("${img[fullRun(title)]}")),
+                      Flexible(child: Container(
+                        width: 25,
+                        height: 25,
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.only(left: 10, right: 10),
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(color: Colors.black.withOpacity(0.25),
+                              blurRadius: 5,
+                              spreadRadius: 5,
+                              offset: const Offset(2.0, 2.0),)
+                          ],
+                          color: Colors.orange.withOpacity(0.75),
+                          borderRadius: const BorderRadius.all(Radius.circular(15)),
+                        ),
+                        child: const Text("SCORE", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
+                      )),
+                      Flexible(child: Container(
+                        width: 350,
+                        height: 350,
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(color: Colors.black.withOpacity(0.75),
+                              blurRadius: 5,
+                              spreadRadius: 5,
+                              offset: const Offset(2.0, 2.0),)
+                          ],
+                          color: Colors.orange.withOpacity(0.75),
+                          shape: BoxShape.circle,
+                          ),
+                        child: Text("${find_score(fullRun(title)).toStringAsFixed(1)}/10", textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold)),
+                        )
+                      ),
+                    Flexible(child: Container(
+                      width: 25,
+                      height: 25,
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(left: 10, right: 10),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(color: Colors.black.withOpacity(0.25),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                            offset: const Offset(2.0, 2.0),)
+                        ],
+                        color: Colors.orange.withOpacity(0.75),
+                        borderRadius: const BorderRadius.all(Radius.circular(15)),
+                      ),
+                      child: const Text("Dollar Per Hour", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
+                    )),
+                    Flexible(child: Container(
+                      width: 100,
+                      height: 100,
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(color: Colors.black.withOpacity(0.75),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                            offset: const Offset(2.0, 2.0),)
+                        ],
+                        color: Colors.orange.withOpacity(0.75),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Text("${dph[fullRun(title)]}/10", textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold)),
+                    )
+                    ),
+                    Flexible(child: Container(
+                      width: 25,
+                      height: 25,
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(left: 10, right: 10),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(color: Colors.black.withOpacity(0.25),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                            offset: const Offset(2.0, 2.0),)
+                        ],
+                        color: Colors.orange.withOpacity(0.75),
+                        borderRadius: const BorderRadius.all(Radius.circular(15)),
+                      ),
+                      child: const Text("Performance", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
+                    )),
+                    Flexible(child: Container(
+                      width: 100,
+                      height: 100,
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(color: Colors.black.withOpacity(0.75),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                            offset: const Offset(2.0, 2.0),)
+                        ],
+                        color: Colors.orange.withOpacity(0.75),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Text("${perf[fullRun(title)]}/10", textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold)),
+                    )
+                    ),
+                    Flexible(child: Container(
+                      width: 25,
+                      height: 25,
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(left: 10, right: 10),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(color: Colors.black.withOpacity(0.25),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                            offset: const Offset(2.0, 2.0),)
+                        ],
+                        color: Colors.orange.withOpacity(0.75),
+                        borderRadius: const BorderRadius.all(Radius.circular(15)),
+                      ),
+                      child: const Text("User Score", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
+                    )),
+                    Flexible(child: Container(
+                      width: 100,
+                      height: 100,
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(color: Colors.black.withOpacity(0.75),
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                            offset: const Offset(2.0, 2.0),)
+                        ],
+                        color: Colors.orange.withOpacity(0.75),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Text("${user[fullRun(title)]}/10", textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold)),
+                    )
+                    ),
+                  ],
+                )
+            ),
+          ));
+    }
+}
+
